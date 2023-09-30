87a398f7b1a80ecf3af8bc95ba60a8a2f05890d2
sejin park <101460733+codesejin@users.noreply.github.com>
2023-09-30 18:50:58 +0900

    Time: 39 ms (30%), Space: 44.6 MB (12.25%) - LeetHub

--- 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.java
+++ 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.java
@@ -6,19 +6,19 @@ class Solution {
             int cx = queries[i][0];
             int cy = queries[i][1];
             int r = queries[i][2];
-            int cnt = 0; // 각 쿼리마다 cnt 변수 초기화
 
+            int cnt = 0;
             for (int j = 0; j < points.length; j++) {
                 int px = points[j][0];
                 int py = points[j][1];
                 
                 // 점 (px, py)가 원 (cx, cy, r) 내부에 있는지 확인
-                double distance = Math.sqrt(Math.pow(px - cx, 2) + Math.pow(py - cy, 2));
-                if (distance <= r) {
+                double distance = Math.pow(px - cx, 2) + Math.pow(py - cy, 2);
+                if (distance <=  Math.pow(r,2)) {
                     cnt++;
                 }
             }
-            answer[i] = cnt; // 결과 저장
+            answer[i] = cnt;
         }
 
         return answer;
