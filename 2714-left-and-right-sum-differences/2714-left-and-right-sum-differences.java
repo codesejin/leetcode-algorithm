b632734d272a69f61a56acbbc678d181ff6db8df
sejin park <101460733+codesejin@users.noreply.github.com>
2023-10-10 18:40:14 +0900

    Time: 2 ms (93.24%), Space: 44.1 MB (12.62%) - LeetHub

--- 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int[] leftRightDifference(int[] nums) {
+public int[] leftRightDifference(int[] nums) {
+    int[] leftSum = new int[nums.length];
+    int[] rightSum = new int[nums.length];
 
-        int[] leftSum = new int[nums.length];
-        int[] rightSum = new int[nums.length];
-        // 왼쪽 합계 계산
-        leftSum[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            leftSum[i] = leftSum[i - 1] + nums[i];
-        }
+    // 왼쪽 합계 계산
+    leftSum[0] = 0;
+    for (int i = 1; i < nums.length; i++) {
+        leftSum[i] = leftSum[i - 1] + nums[i - 1];
+    }
 
-        // 오른쪽 합계 계산
-        rightSum[nums.length - 1] = nums[nums.length - 1];
-        for (int i = nums.length - 2; i >= 0; i--) {
-            rightSum[i] = rightSum[i + 1] + nums[i];
-        }
+    // 오른쪽 합계 계산
+    rightSum[nums.length - 1] = 0;
+    for (int i = nums.length - 2; i >= 0; i--) {
+        rightSum[i] = rightSum[i + 1] + nums[i + 1];
+    }
 
-        // 결과 배열 계산
-        int[] result = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            result[i] = Math.abs(leftSum[i] - rightSum[i]);
-        }
+    // 결과 배열 계산
+    int[] result = new int[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+        result[i] = Math.abs(leftSum[i] - rightSum[i]);
+    }
 
-        return result;
+    return result;
     }
 }
