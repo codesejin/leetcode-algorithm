da80244bac4138541cd8c144a232ee72cf7741ac
sejin park <101460733+codesejin@users.noreply.github.com>
2024-05-27 09:19:33 +0900

    Time: 4 ms (14.85%), Space: 41.2 MB (95.46%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,21 +1,20 @@
 class Solution {
     public int calPoints(String[] operations) {
 
-        List<String> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < operations.length; i++) {
             String cur = operations[i];
 
             if (cur.equals("C")) {
                 list.remove(list.size() - 1);
             } else if (cur.equals("D")) {
-                list.add(String.valueOf(Integer.parseInt(list.get(list.size() - 1)) * 2));
+                list.add(list.get(list.size() - 1) * 2);
             } else if (cur.equals("+")) {
-                list.add(String.valueOf(Integer.parseInt(list.get(list.size() - 1)) + Integer.parseInt(list.get(list.size() - 2))));
+                list.add(list.get(list.size() - 1) + list.get(list.size() - 2));
             } else {
-
-                list.add(cur);
+                list.add(Integer.parseInt(cur));
             }
         }
-        return list.stream().mapToInt(Integer::parseInt).sum();
+        return list.stream().mapToInt(i -> i).sum();
     }
 }
