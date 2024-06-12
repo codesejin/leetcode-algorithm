9480d511fa3487627a417b4ae544e83b67c44fd9
sejin park <101460733+codesejin@users.noreply.github.com>
2024-06-12 11:00:30 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (42.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,41 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+        // Arrays.sort(nums);
+//         int left = 0;
+//         for(int i = 1; i < nums.length; i++) {
+//             int cur = nums[i];
+//             System.out.println(" >> i : " + i + " , cur : " + cur);
+//              System.out.println(" >> left : " + left + " , nums[left]  : " + nums[left]);
+//            if (nums[left] > cur) {
+//                int tmp = nums[left];
+//                nums[left] = cur;
+//                nums[i] = tmp;
+//                left++;
+//            }
+//             System.out.println(Arrays.toString(nums));
+
+//         }
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // Swap nums[low] and nums[mid]
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                // Swap nums[mid] and nums[high]
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
     }
 }
