6adc2a8843f7c79dc57e1707a787ef96b112f914
sejin park <101460733+codesejin@users.noreply.github.com>
2024-07-02 09:10:33 +0900

    Time: 7 ms (6.39%), Space: 42.4 MB (5.03%) - LeetHub

---
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -0,0 +1,56 @@
+class Solution {
+   // static List<Coordinate> list;
+    
+    
+    public int oddCells(int m, int n, int[][] indices) {
+        int[][] twoDArray = new int[m][n];
+        
+       // list = new ArrayList<>();
+        
+        HashMap<String, List<Integer>> map = new HashMap<>();
+        map.putIfAbsent("X", new ArrayList<>());
+        map.putIfAbsent("Y", new ArrayList<>());
+
+        for (int i = 0; i < indices.length; i++) {
+            int x = indices[i][0];
+            int y = indices[i][1];
+            //list.add(new Coordinate("X"), x);
+            //list.add(new Coordinate("Y"), x);
+            
+            map.get("X").add(x);
+            map.get("Y").add(y);
+        }
+        // x측 - row측 증가시키기
+        for (Integer x : map.get("X")) {
+          for (int i = 0; i < n; i++) {
+              twoDArray[x][i] += 1;
+          }
+        }
+        
+        // y측 - column측 증가시키기
+        for (Integer y : map.get("Y")) {
+          for (int i = 0; i < m; i++) {
+              twoDArray[i][y] += 1;
+          }
+        }
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j % 2 != 0) {
+                    result += twoDArray[i][j];
+                }
+            }
+        }
+        return (int)Arrays.stream(twoDArray).flatMapToInt(Arrays::stream).filter(num -> num % 2 != 0).count();
+    }
+    
+//     static Class Coordinate {
+//         String location;
+//         int idx;
+        
+//         public coordinate ( String location, int idx ) {
+//             this.location = location;
+//             this.idx = idx;
+//         }
+//     }
+}
