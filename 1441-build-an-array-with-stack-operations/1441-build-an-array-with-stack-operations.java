d443221702d4a1af20740834dccf4ab2e704cfa4
sejin park <101460733+codesejin@users.noreply.github.com>
2024-07-11 08:49:51 +0900

    Time: 0 ms (100.00%), Space: 42.6 MB (41.29%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -5,13 +5,13 @@ class Solution {
     public List<String> buildArray(int[] target, int n) {
         List<String> result = new ArrayList<>();
         Stack<Integer> st = new Stack<>();
-        int targetIndex = 0;
+        int targetIdx = 0;
 
-        for (int i = 1; i <= n && targetIndex < target.length; i++) {
+        for (int i = 1; i <= n && targetIdx < target.length; i++) {
             st.push(i);
             result.add(PUSH);
-            if (i == target[targetIndex]) {
-                targetIndex++;
+            if (i == target[targetIdx]) {
+                targetIdx++;
             } else {
                 st.pop();
                 result.add(POP);
