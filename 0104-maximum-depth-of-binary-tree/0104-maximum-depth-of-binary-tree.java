2073b8813bc519f11c8715f56177a0290598cdc9
sejin park <101460733+codesejin@users.noreply.github.com>
2024-08-22 10:12:42 +0900

    Time: 1 ms (11.70%), Space: 42.2 MB (87.61%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,30 +15,30 @@
  */
 class Solution {
     // 더 멀리 있는 리프노드까지의 가장긴 길이를 구하라 -> bfs
+    static int level;
     public int maxDepth(TreeNode root) {
+        level = 0;
         if (root == null) {
             return 0;
         }
-
+        bfs(root);
+        return level;
+    }
+    private void bfs(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        int depth = 0;
-
         while (!queue.isEmpty()) {
             int size = queue.size();
-
             for (int i = 0; i < size; i++) {
-                TreeNode currentNode = queue.poll();
-                if (currentNode.left != null) {
-                    queue.offer(currentNode.left);
+                TreeNode cur = queue.poll();
+                if (cur.left != null) {
+                    queue.offer(cur.left);
                 }
-                if (currentNode.right != null) {
-                    queue.offer(currentNode.right);
+                if (cur.right != null) {
+                    queue.offer(cur.right);
                 }
             }
-            depth++;
+            level++;
         }
-
-        return depth;
     }
 }
