f7bcbcd5fa76ad2f37a84dca29c73c235917c78d
sejin park <101460733+codesejin@users.noreply.github.com>
2024-09-08 14:50:49 +0900

    Time: 12 ms (46.62%), Space: 44.1 MB (83.08%) - LeetHub

---
+++ 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String addBoldTag(String s, String[] words) {
+        boolean[] bold = new boolean[s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            for (String word : words) {
+                
+                if(s.startsWith(word, i)) {
+                    for (int j = 0; j < word.length(); j++) {
+                        bold[j + i] = true;
+                    }
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+        
+        while (idx < s.length()) {
+            if (!bold[idx]) {
+                sb.append(s.charAt(idx++));
+                continue;
+            }
+            sb.append("<b>");
+            while (idx < s.length() && bold[idx]) {
+                sb.append(s.charAt(idx++));
+            }
+            sb.append("</b>");
+        }
+        return sb.toString();
+    }
+}
