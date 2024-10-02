4ade4ae6607215717caacf9c0735b8adf2ec9652
sejin park <101460733+codesejin@users.noreply.github.com>
2024-10-02 11:19:55 +0900

    Time: 3 ms (85.94%), Space: 49.3 MB (72.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,6 +2,7 @@ class Solution {
     // 1 : 땅
     // 0 : 물
     // 연결되어있지 않은 땅의 개수 반환
+
     int[] dx = {1, -1, 0, 0};
     int[] dy = {0, 0, 1, -1};
 
@@ -21,21 +22,17 @@ class Solution {
 
     private void dfs(int x, int y, char[][] grid) {
         grid[x][y] = 0;
-        // 네 방향 탐색
+
         for (int i = 0; i < 4; i++) {
             int newX = x + dx[i];
             int newY = y + dy[i];
-
-            // 경계값 확인 및 방문 여부와 땅인 경우만 재귀 호출
-            if (validateIndex(newX, newY, grid.length, grid[0].length) 
-                && grid[newX][newY] == '1') {
+            if(validatedIndex(newX, newY, grid) && grid[newX][newY] == '1') {
                 dfs(newX, newY, grid);
             }
         }
     }
 
-    // 배열의 인덱스가 유효한지 확인하는 메서드
-    private boolean validateIndex(int newX, int newY, int rowLen, int colLen) {
-        return newX >= 0 && newX < rowLen && newY >= 0 && newY < colLen;
+    private boolean validatedIndex(int newX, int newY, char[][] grid) {
+        return newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length;
     }
 }
