38a52453d5f8c69ec234d14d466a9ca2d5287021
sejin park <101460733+codesejin@users.noreply.github.com>
2024-10-05 00:05:11 +0900

    Time: 9 ms (39.25%), Space: 43.3 MB (50.75%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        // 연속된 공백을 하나의 구분자로 처리
+        String[] splited = s.trim().split("\\s+");
+        System.out.println(Arrays.deepToString(splited));
+
+        // 배열을 뒤에서부터 순서대로 append
+        for (int i = splited.length - 1; i >= 0; i--) {
+            sb.append(splited[i]);
+
+            // 마지막 단어가 아닌 경우만 공백 추가
+            if (i > 0) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+}
